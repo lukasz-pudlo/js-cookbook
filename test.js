0ae8da9b091313861c741217387242a8fda5f897
lukasz <l.pudlo1990@gmail.com>
2022-06-13 23:59:04 +0100

    working with numbers

--- test.js
+++ test.js
@@ -10,3 +10,15 @@ function isString() {
         alert("This is not a string")
     )
 }
+
+function createObjectString() {
+    const unknownVariable = new String('this an object string');
+    console.log(unknownVariable);
+    console.log(String.prototype.isPrototypeOf(unknownVariable));
+}
+
+function numberToFixed() {
+    var inputNumber = document.getElementById("text-input").value;
+    const someNumber = inputNumber.toFixed(2);
+    console.log(someNumber);
+}
