498a17ff47722e222dfd99ae5a09540020ca6cf6
lukasz <l.pudlo1990@gmail.com>
2022-07-08 23:46:49 +0100

    continuing with objects javascript cookbook

--- scraper.js
+++ scraper.js
@@ -1,13 +1,22 @@
+// document.getElementById('scrape').addEventListener("click", scrapePage);
+
+// const browser = await puppeteer.launch({
+//     userDataDir: './data', 
+// })
+
+// const word = document.getElementById('wordToSearch').value;
+
+const word = 'ordinateur';
 const puppeteer = require('puppeteer');
 function run () {
     return new Promise(async (resolve, reject) => {
         try {
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
-            await page.goto("https://cnrtl.fr/definition/commencer");
+            await page.goto(`https://cnrtl.fr/definition/${word}`);
             let urls = await page.evaluate(() => {
                 let results = [];
-                let items = document.querySelectorAll('span.tlf_cdefinition');
+                let items = document.querySelectorAll('div.tlf_parah');
                 items.forEach((item) => {
                     results.push({
                         text: item.innerText,
